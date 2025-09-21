2e235d3733d2ea6b7c800c59adfb63c983f91ebc
Ethan <180671720+3thanlarson@users.noreply.github.com>
2025-09-21 17:50:42 -0500

    Implement getComputerChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,9 @@
-console.log("Wazzuuuup");
+function getComputerChoice() {
+    var randomNumber = Math.random();
+    if (randomNumber < .33) {
+        return "rock" }
+    else if (randomNumber < .66 && randomNumber >=.33) {
+        return "paper" }
+    else {
+        return "scissors"};
+}
